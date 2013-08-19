14a722838ee8a66929f3e76c18503b3c94e7de83
fixanoid <felix.shnir@gmail.com>
2013-08-19 02:07:20 -0400

    json editor

--- data/js/sse-viewer.js
+++ data/js/sse-viewer.js
@@ -1,3 +1,5 @@
+var editor;
+
 self.port.on('list-addon', function (data) {
 	var contents = document.getElementById('contents');
 
@@ -7,12 +9,44 @@ self.port.on('list-addon', function (data) {
  	var a = document.createElement('a');
  	a.id = data.id;
  	a.href = '#';
+ 	a.addEventListener('click', function (e) {
+ 		self.port.emit('get-ss', {id: this.id});
+		e.preventDefault();
+	});
 
  	a.innerHTML = data.id;
 
  	listing.appendChild(a);
 
 	contents.appendChild(listing);
+});
+
+self.port.on('get-json', function(data) {
+	document.getElementById('contents').style.display = 'none';
+	document.getElementById('editor').style.display = '';
+
+	var container = document.getElementById("json-editor");
+
+	if (!editor) {
+		editor = new jsoneditor.JSONEditor(container);
+	}
+
+	editor.set(JSON.parse(data.json));
+
+	document.getElementById('save-json').addEventListener('click', function (e) {
+ 		var json = editor.get();
+ 
+ 		// TODO
+
+		e.preventDefault();
+	});
+
+	document.getElementById('cancel').addEventListener('click', function (e) {
+		document.getElementById('editor').style.display = 'none';
+ 		document.getElementById('contents').style.display = '';
+
+		e.preventDefault();
+	});
 });
 
 self.port.on('no-addons', function (data) {
