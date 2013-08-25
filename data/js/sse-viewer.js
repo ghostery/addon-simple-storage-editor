7f2b0f8d3d9006dc9f2641d65cdf4d530fa02085
fixanoid <felix.shnir@gmail.com>
2013-08-25 00:17:57 -0400

    UI

--- data/js/sse-viewer.js
+++ data/js/sse-viewer.js
@@ -5,12 +5,26 @@
 var editor;
 
 self.port.on('list-addon', function (data) {
-	var contents = document.getElementById('addon-list');
+	var contents = document.getElementById('extensions-tbody');
 
-	var listing = document.createElement('p');
+	var row = document.createElement('tr'),
+	 cell = document.createElement('td');
 
- 	listing.appendChild(document.createTextNode(data.name + ' '));
- 	var a = document.createElement('a');
+	cell.id = 'addon-name-' + data.id;
+	cell.appendChild(document.createTextNode(data.name));
+	row.appendChild(cell);
+
+	cell = document.createElement('td');
+	cell.appendChild(document.createTextNode(data.id));
+	row.appendChild(cell);
+
+	cell = document.createElement('td');
+	cell.id = 'status-' + data.id;
+	cell.appendChild(document.createTextNode(!data.status));
+	row.appendChild(cell);
+
+	cell = document.createElement('td');
+	var a = document.createElement('a');
  	a.id = data.id;
  	a.href = '#';
  	a.addEventListener('click', function (e) {
@@ -18,18 +32,37 @@ self.port.on('list-addon', function (data) {
 		e.preventDefault();
 	});
 
- 	a.innerHTML = data.id;
+	a.appendChild(document.createTextNode('Edit Storage'));
 
- 	listing.appendChild(a);
+	cell.appendChild(a);
 
-	contents.appendChild(listing);
+	a = document.createElement('a');
+ 	a.id = data.id;
+ 	a.href = '#';
+ 	a.addEventListener('click', function (e) {
+ 		self.port.emit('toggle-addon', {id: this.id});
+		e.preventDefault();
+	});
+
+	cell.appendChild(document.createTextNode(' | '));
+	a.appendChild(document.createTextNode('Toggle Addon'));
+
+	cell.appendChild(a);
+	row.appendChild(cell);
+
+	contents.appendChild(row);
+});
+
+self.port.on('toggle-addon-complete', function(data) {
+	var status = document.getElementById('status-' + data.id);
+	status.innerHTML = !data.status;
 });
 
 self.port.on('get-json', function(data) {
 	document.getElementById('contents').style.display = 'none';
 	document.getElementById('editor').style.display = '';
 
-	var container = document.getElementById("json-editor");
+	var container = document.getElementById('json-editor');
 
 	if (!editor) {
 		editor = new jsoneditor.JSONEditor(container);
@@ -37,13 +70,19 @@ self.port.on('get-json', function(data) {
 
 	editor.set(JSON.parse(data.json));
 
+	document.getElementById('editor-addon-name').innerHTML = document.getElementById('addon-name-' + data.id).innerHTML  + ' (' + data.id + ')';
+
 	document.getElementById('save-json').addEventListener('click', function (e) {
- 		var json = editor.get();
- 
- 		var result = confirm('In order to save simple-storage for an addon,\n' +
- 							  'the addon will be disabled, new json will be \n' +
- 							  'saved and the addon will be re-enabled.\n\n' +
- 							  'Press OK to allow, Cancel to stop.');
+ 		var json = editor.get(),
+ 		 status = document.getElementById('status-' + data.id),
+ 		 result = true;
+
+		if (status.innerHTML == 'true') { 
+ 			result = confirm('In order to save simple-storage for an addon,\n' +
+ 							 'the addon will be disabled, new json will be \n' +
+ 							 'saved and the addon will be re-enabled.\n\n' +
+ 							 'Press OK to allow, Cancel to stop.');
+ 		}
 
  		if (result == true) {
 	 		self.port.emit('send-ss', {
@@ -51,6 +90,9 @@ self.port.on('get-json', function(data) {
 	 			json: json
 	 		});
 	 	}
+
+		document.getElementById('editor').style.display = 'none';
+ 		document.getElementById('contents').style.display = '';
 
 		e.preventDefault();
 	});
